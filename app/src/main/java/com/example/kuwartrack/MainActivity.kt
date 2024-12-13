a036a80a1642554911df85e78f512f2c5014963f
psychammer <psychammer87@gmail.com>
2024-12-14 00:15:13 +0800

    first change

---
+++ app/src/main/java/com/example/kuwartrack/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.kuwartrack
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnLogin : ImageButton = findViewById(R.id.login_button)
+
+        btnLogin.setOnClickListener{
+
+            val Intent = Intent(this, HomeActivity::class.java)
+            startActivity(Intent)
+
+        }
+
+    }
+
+}
